a681dbb6fd3917aa8dd4df2eb0fc46f1433a2940
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-02-25 14:28:17 +0100

    Added BinaryIndexTree

--- App/Program.cs
+++ App/Program.cs
@@ -9,12 +9,25 @@ namespace App
     class Program
     {
         static void Main()
-        {
+        {/*
             BinarySearchTree<int> tree = new();
             tree.Insert(3);
             tree.Insert(1);
             tree.InsertMany(new List<int> {6, 2, 4});
-            Print(tree);
+            Print(tree);*/
+
+            int[] array = {2, 1, 1, 3, 2, 3,
+                    4, 5, 6, 7, 8, 9};
+            int n = array.Length;
+            BinaryIndexTree tree = new BinaryIndexTree(100);
+            tree.ConstructTree(array, n);
+
+            Console.WriteLine("Sum of elements in arr[0..5]" +
+                            " is " + tree.GetSum(5));
+            array[3] += 6;
+            tree.UpdateTree(n, 3, 6);
+            Console.WriteLine("Sum of elements in arr[0..5]" +
+                        " after update is " + tree.GetSum(5));
         }
 
         public static void Print(BinarySearchTree<int> tree)
